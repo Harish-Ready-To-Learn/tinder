d40473605ed9e9a301e2b45b9f62e5b773af7c51
Harish <harish.v@heptagon.in>
2023-08-22 21:16:37 +0530

    message

---
+++ source/components/SenderMessage.js
@@ -0,0 +1,19 @@
+import { View, Text } from "react-native";
+import React from "react";
+import tw from "tailwind-react-native-classnames";
+
+const SenderMessage = ({ message }) => {
+  console.log("sender", message);
+  return (
+    <View
+      style={tw.style(
+        "bg-purple-600 rounded-lg rounded-tr-none px-5 py-3 mx-3 my-2",
+        { alignSelf: "flex-start", marginLeft: "auto" }
+      )}
+    >
+      <Text style={tw.style("text-white")}>{message.message}</Text>
+    </View>
+  );
+};
+
+export default SenderMessage;
